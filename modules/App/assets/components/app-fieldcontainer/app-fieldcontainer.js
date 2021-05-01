03f808881123c4fa35350fca354391306dac8bf4
Artur Heinze <artur@agentejo.com>
2021-05-01 02:53:30 +0200

    fix field-container when nested

--- modules/App/assets/components/app-fieldcontainer/app-fieldcontainer.js
+++ modules/App/assets/components/app-fieldcontainer/app-fieldcontainer.js
@@ -1,6 +1,13 @@
 const fn = e => {
 
-    let closest = e.target.closest('app-fieldcontainer');
+    let element = e.target, closest = e.target.matches('app-fieldcontainer') ? e.target : null;
+
+    while ((element = element.parentElement)) {
+        if (element.matches('app-fieldcontainer')) {
+            closest = element
+        }
+    }
+
     let containers = document.querySelectorAll('app-fieldcontainer');
 
     containers.forEach(container => {
