2c919fb65ec48e243df6cd4d969c5a969cdb0d63
Artur Heinze <artur@agentejo.com>
2021-05-13 00:45:54 +0200

    update item state

--- modules/Content/views/collection/item.php
+++ modules/Content/views/collection/item.php
@@ -32,16 +32,31 @@
                 </div>
                 <div class="kiss-width-1-4@m kiss-width-1-5@xl">
 
+                    <div class="kiss-margin">
+
+                        <div class="kiss-text-bold kiss-size-xsmall kiss-text-upper">{{ t('State') }}</div>
+
+                        <div class="kiss-margin-small">
+                            <button type="button" class="kiss-button kiss-flex kiss-flex-middle kiss-width-expand kiss-align-left" :class="{'kiss-bgcolor-danger': !item._state, 'kiss-bgcolor-success': item._state == 1}" kiss-popoutmenu="#model-item-menu-state">
+                                <span class="kiss-flex-1" v-if="item._state == 1">{{ t('Published') }}</span>
+                                <span class="kiss-flex-1" v-if="!item._state">{{ t('Unpublished') }}</span>
+                                <span class="kiss-flex-1" v-if="item._state == -1">{{ t('Archive') }}</span>
+                                <icon>expand_more</icon>
+                            </button>
+                        </div>
+
+                    </div>
+
                     <div class="kiss-margin" v-if="hasLocales">
 
                         <div class="kiss-text-bold kiss-size-xsmall kiss-text-upper">{{ t('Localeizations') }}</div>
 
-                        <kiss-card class="kiss-padding-small kiss-margin kiss-text-bolder kiss-text-muted kiss-size-small kiss-color-muted kiss-flex kiss-flex-middle" theme="bordered" v-if="!locales.length">
+                        <kiss-card class="kiss-padding-small kiss-margin-small kiss-text-bolder kiss-text-muted kiss-size-small kiss-color-muted kiss-flex kiss-flex-middle" theme="bordered" v-if="!locales.length">
                             <span class="kiss-flex-1 kiss-margin-small-right">{{ t('No locales.') }}</span>
                             <a class="kiss-size-xsmall" href="<?=$this->route('/settings/locales')?>">{{ t('Manage') }}</a>
                         </kiss-card>
 
-                        <div class="kiss-margin" v-if="locales.length">
+                        <div class="kiss-margin-small" v-if="locales.length">
 
                             <kiss-card class="kiss-position-relative kiss-padding-small kiss-margin-small kiss-text-bolder kiss-flex kiss-flex-middle" :class="{'kiss-color-muted': !loc.visible}" theme="bordered" v-for="loc in locales">
                                 <icon class="kiss-margin-small-right" :class="{'kiss-color-primary': loc.visible}">{{ loc.visible ? 'visibility' : 'visibility_off' }}</icon>
@@ -80,6 +95,34 @@
                 </kiss-container>
 
             </app-actionbar>
+
+            <kiss-popoutmenu id="model-item-menu-state">
+                <kiss-content>
+                    <kiss-navlist class="kiss-margin">
+                        <ul>
+                            <li class="kiss-nav-header"><?=t('Change status to')?></li>
+                            <li v-show="item._state != 1">
+                                <a class="kiss-flex kiss-flex-middle kiss-color-success kiss-text-bold" @click="item._state=1">
+                                    <icon class="kiss-margin-small-right">bookmark</icon>
+                                    <?=t('Published')?>
+                                </a>
+                            </li>
+                            <li v-show="item._state">
+                                <a class="kiss-flex kiss-flex-middle kiss-color-danger kiss-text-bold" @click="item._state=0">
+                                    <icon class="kiss-margin-small-right">bookmark</icon>
+                                    <?=t('Unpublished')?>
+                                </a>
+                            </li>
+                            <li v-show="item._state != -1">
+                                <a class="kiss-flex kiss-flex-middle kiss-color-muted kiss-text-bold" @click="item._state=-1">
+                                    <icon class="kiss-margin-small-right">bookmark</icon>
+                                    <?=t('Archive')?>
+                                </a>
+                            </li>
+                        </ul>
+                    </kiss-navlist>
+                </kiss-content>
+            </kiss-popoutmenu>
 
             <kiss-popoutmenu id="model-item-menu-actions">
                 <kiss-content>
