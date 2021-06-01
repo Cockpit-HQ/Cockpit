e9654c735b3c0e4f485f2f30b1faa94aace74eff
Artur Heinze <artur@agentejo.com>
2021-06-01 16:02:09 +0200

    add filter function to layout components view

--- modules/Layout/views/index.php
+++ modules/Layout/views/index.php
@@ -22,25 +22,44 @@
                 </div>
             </div>
 
-            <ul class="app-list-items animated fadeIn" v-if="components && components.length">
+            <div v-if="components && components.length">
 
-                <li v-for="(component, idx) in components">
+                <app-tabs static="true" v-if="groups.length">
+                    <ul class="app-tabs-nav">
+                        <li :active="group === null">
+                            <a class="app-tabs-nav-link" @click="group = null">{{t('All')}}</a>
+                        </li>
+                        <li :active="group == name" v-for="name in groups">
+                            <a class="app-tabs-nav-link" @click="group = name">{{ name }}</a>
+                        </li>
+                    </ul>
+                </app-tabs>
 
-                    <div class="kiss-margin kiss-flex kiss-flex-middle">
-                        <div class="kiss-margin-small-right">
-                            <kiss-svg :src="$base('layout:assets/icons/component.svg')" width="30" height="30"></kiss-svg>
+                <div class="kiss-margin">
+                    <input type="text" class="kiss-input" :placeholder="t('Filter components...')" v-model="filter">
+                </div>
+
+                <ul class="app-list-items animated fadeIn">
+
+                    <li v-for="(component, idx) in filtered">
+
+                        <div class="kiss-margin kiss-flex kiss-flex-middle">
+                            <div class="kiss-margin-small-right">
+                                <kiss-svg :src="$base('layout:assets/icons/component.svg')" width="30" height="30"></kiss-svg>
+                            </div>
+                            <div class="kiss-size-5 kiss-flex-1 kiss-position-relative">
+                                <strong>{{component.meta.label || component.name}}</strong>
+                                <a class="kiss-cover" :href="$route('/layout-components/component/'+component._id)"></a>
+                            </div>
+                            <div class="kiss-color-muted kiss-size-small">{{component.meta.group}}</div>
+                            <a class="kiss-display-block kiss-margin-left kiss-color-danger" @click="remove(component)"><icon>delete</icon></a>
                         </div>
-                        <div class="kiss-size-5 kiss-flex-1 kiss-position-relative">
-                            <strong>{{component.meta.label || component.name}}</strong>
-                            <a class="kiss-cover" :href="$route('/layout-components/component/'+component._id)"></a>
-                        </div>
-                        <div class="kiss-color-muted kiss-size-small">{{component.meta.group}}</div>
-                        <a class="kiss-display-block kiss-margin-left kiss-color-danger" @click="remove(component)"><icon>delete</icon></a>
-                    </div>
 
-                </li>
+                    </li>
 
-            </ul>
+                </ul>
+
+            </div>
 
             <app-actionbar>
 
@@ -61,7 +80,40 @@
                 data() {
                     return {
                         components: null,
+                        group: null,
+                        filter: '',
                         loading: false
+                    }
+                },
+
+                computed: {
+
+                    filtered() {
+
+                        let components = [];
+
+                        (this.components || []).forEach(c => {
+
+                            if (this.filter && !`${c.name} ${c.meta.label || ''}`.toLocaleLowerCase().includes(this.filter.toLocaleLowerCase())) {
+                                return;
+                            }
+
+                            if (this.group && c.meta.group != this.group ) return;
+                            components.push(c)
+                        });
+
+                        return components;
+                    },
+
+                    groups() {
+                        let groups = [];
+
+                        (this.components || []).forEach(c => {
+                            if (!c.meta.group || groups.indexOf(c.meta.group) > -1) return;
+                            groups.push(c.meta.group);
+                        });
+
+                        return groups.sort();
                     }
                 },
 
