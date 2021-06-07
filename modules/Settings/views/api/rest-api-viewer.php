179bc9b08f2b3bde34276ff41d92652621191dde
Artur Heinze <artur@agentejo.com>
2021-06-07 18:23:48 +0200

    fix rest-api viewer

--- modules/Settings/views/api/rest-api-viewer.php
+++ modules/Settings/views/api/rest-api-viewer.php
@@ -3,19 +3,21 @@
 <head>
 
   <script type="module">
-    <?php include($this->path('settings:assets/vendor/rapidoc.js')) ?>
+    import "<?=($this->pathToUrl('settings:assets/vendor/rapidoc.js')) ?>";
   </script>
-</head>
-  <body>
-    <rapi-doc
-    spec-url="<?=$openApiUrl?>"
-    show-header="false"
-    show-info="false"
-    render-style="read"
 
-    bg-color="#10131a"
-    text-color="#fafafa"
-    primary-color="#0e8fff"
+</head>
+<body>
+    <rapi-doc
+      spec-url="<?=$openApiUrl?>"
+      show-header="false"
+      show-info="false"
+      render-style="read"
+      load-fonts="false"
+
+      bg-color="#10131a"
+      text-color="#fafafa"
+      primary-color="#0e8fff"
     ></rapi-doc>
-  </body>
+</body>
 </html>
