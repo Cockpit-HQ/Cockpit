55327d8b613917a4d6b334f2098b0036e97d7956
Artur Heinze <artur@agentejo.com>
2021-07-24 00:59:45 +0200

    refactor api route endpoints

--- modules/App/api.php
+++ modules/App/api.php
@@ -21,55 +21,6 @@ $this->service('gql', function() use($app) {
     return $gql;
 });
 
-$this->bind('/api/gql', function() {
-
-    $query = $this->param('query', '{}');
-    $variables = $this->param('variables', null);
-    $contentType = $this->request->server['CONTENT_TYPE'] ?? ($this->request->server['HTTP_CONTENT_TYPE'] ?? '');
-
-    if (stripos($contentType, 'multipart/form-data') !== false) {
-
-        /**
-         * Query with upload
-         * https://github.com/jaydenseric/graphql-multipart-request-spec
-         */
-
-        $map = $this->param('map', '{}');
-        $operations = $this->param('operations');
-
-        $result = json_decode($operations, true);
-        $map = json_decode($map, true);
-
-        if (isset($result['operationName'])) {
-            $result['operation'] = $result['operationName'];
-            unset($result['operationName']);
-        }
-
-        $query = $result['query'] ?? '';
-        $variables = $result['variables'] ?? [];
-
-        foreach ($map as $fileKey => $locations) {
-
-            foreach ($locations as $location) {
-
-                $parts = explode('.', $location);
-
-                if ($parts[0] == 'variables') array_shift($parts);
-
-                $v = &$variables;
-
-                foreach ($parts as $key) {
-                    if (!isset($v[$key]) || !is_array($v[$key])) $v[$key] = [];
-                    $v = &$v[$key];
-                }
-                $v = $_FILES[$fileKey];
-            }
-        }
-    }
-
-    return $this->gql->process($query, $variables);
-});
-
 // Rest Api service
 $this->service('restApi', function() use($app) {
     $restApi = new App\RestApi\Query($app);
@@ -78,6 +29,54 @@ $this->service('restApi', function() use($app) {
 
 $this->bind('/api/*', function($params) {
 
+    //graphql query
+    if ($params[':splat'][0] == 'gql') {
+
+        $query = $this->param('query', '{}');
+        $variables = $this->param('variables', null);
+        $contentType = $this->request->server['CONTENT_TYPE'] ?? ($this->request->server['HTTP_CONTENT_TYPE'] ?? '');
+
+        if (stripos($contentType, 'multipart/form-data') !== false) {
+
+            /**
+             * GQL query with upload: https://github.com/jaydenseric/graphql-multipart-request-spec
+             */
+
+            $map = $this->param('map', '{}');
+            $operations = $this->param('operations');
+
+            $result = json_decode($operations, true);
+            $map = json_decode($map, true);
+
+            if (isset($result['operationName'])) {
+                $result['operation'] = $result['operationName'];
+                unset($result['operationName']);
+            }
+
+            $query = $result['query'] ?? '';
+            $variables = $result['variables'] ?? [];
+
+            foreach ($map as $fileKey => $locations) {
+
+                foreach ($locations as $location) {
+                    $parts = explode('.', $location);
+                    if ($parts[0] == 'variables') array_shift($parts);
+                    $v = &$variables;
+
+                    foreach ($parts as $key) {
+                        if (!isset($v[$key]) || !is_array($v[$key])) $v[$key] = [];
+                        $v = &$v[$key];
+                    }
+
+                    $v = $_FILES[$fileKey];
+                }
+            }
+        }
+
+        return $this->gql->process($query, $variables);
+    }
+
+    // rest api query
     $path = '/'.$params[':splat'][0];
 
     return $this->restApi->process($path, $this->request->method);
