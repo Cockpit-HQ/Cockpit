5ab558b86964180a4c5a6d9d26e9d587398c4731
Artur Heinze <artur@agentejo.com>
2021-09-01 15:44:58 +0200

    more work on lokalize api

--- modules/Lokalize/Controller/Utils.php
+++ modules/Lokalize/Controller/Utils.php
@@ -15,52 +15,15 @@ class Utils extends App {
 
         $text = $this->param('text');
         $to = $this->param('to');
-        $apiKey = $this->app->retrieve('lokalize/translation/apiKey');
-
-        if (!$apiKey) {
-            return ['translation' => ''];
-        }
 
         if (!$text || !$to) {
             return false;
         }
 
-        $ch = curl_init();
+        $translation = $this->module('lokalize')->translate($text, $to);
 
-        $data = [
-            'auth_key' => $apiKey,
-            'text' => $text,
-            'target_lang' => $to
-        ];
-
-        curl_setopt($ch, CURLOPT_URL, 'https://api-free.deepl.com/v2/translate');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-
-        $error = null;
-        $headers = [];
-        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-        $result = curl_exec($ch);
-
-        if (curl_errno($ch)) {
-            $error = ['error' => curl_error($ch)];
-        }
-
-        curl_close($ch);
-
-        if ($error) {
-            return $error;
-        }
-
-        $json = \json_decode($result, true);
-        $translation = '';
-
-        if (isset($json['translations'][0]['text'])) {
-            $translation = $json['translations'][0]['text'];
+        if (!$translation) {
+            $translation = '';
         }
 
         return compact('translation');
--- modules/Lokalize/bootstrap.php
+++ modules/Lokalize/bootstrap.php
@@ -102,6 +102,59 @@ $this->module('lokalize')->extend([
         }
 
         return $values;
+    },
+
+    'translate' => function(string $text, string $to) {
+
+        $apiKey = $this->app->retrieve('lokalize/translation/apiKey');
+
+        if (!$apiKey) {
+            return ['error' => 'Translation api key is missing'];
+        }
+
+        if (!$text || !$to) {
+            return false;
+        }
+
+        $ch = curl_init();
+
+        $data = [
+            'auth_key' => $apiKey,
+            'text' => $text,
+            'target_lang' => $to
+        ];
+
+        curl_setopt($ch, CURLOPT_URL, 'https://api-free.deepl.com/v2/translate');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+        $error = null;
+        $headers = [];
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            $error = ['error' => curl_error($ch)];
+        }
+
+        curl_close($ch);
+
+        if ($error) {
+            return $error;
+        }
+
+        $json = \json_decode($result, true);
+        $translation = '';
+
+        if (isset($json['translations'][0]['text'])) {
+            $translation = $json['translations'][0]['text'];
+        }
+
+        return $translation;
     }
 
 ]);
