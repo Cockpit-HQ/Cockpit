801ef694c92cf064cbdb95debd226410e3a97c4b
Artur Heinze <artur@agentejo.com>
2021-09-03 22:50:44 +0200

    remove addons from core

--- modules/Layout/assets/js/layout.js
+++
@@ -1,2 +0,0 @@
-// Global Vue components
-VueView.component('field-layout', 'layout:assets/vue-components/field-layout.js');
--- modules/Webhooks/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// load admin related code
-$this->on('app.admin.init', function() {
-    include(__DIR__.'/admin.php');
-});
