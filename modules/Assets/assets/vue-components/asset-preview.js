770774f81030eef1bfb61ccad2f2be29d29c24f6
Artur Heinze <artur@agentejo.com>
2021-12-15 12:50:06 +0100

    fix too small svg previews in asset manager

--- modules/Assets/assets/vue-components/asset-preview.js
+++ modules/Assets/assets/vue-components/asset-preview.js
@@ -31,7 +31,7 @@ export default {
     template: /*html*/`
         <div>
             <div class="kiss-cover kiss-flex kiss-flex-middle kiss-flex-center" v-if="asset.type=='image'">
-                <img class="animated fadeIn kiss-margin-auto kiss-responsive-height" loading="lazy" :src="$route('/assets/thumbnail/'+asset._id+'?m=bestFit&mime=auto&h=300&t='+asset._modified)" v-if="!loading">
+                <img class="animated fadeIn kiss-margin-auto kiss-responsive-height" loading="lazy" :src="$route('/assets/thumbnail/'+asset._id+'?m=bestFit&mime=auto&h=300&t='+asset._modified)" :style="{minHeight: asset.mime == 'image/svg+xml' ? '60%':''}" v-if="!loading">
                 <app-loader size="small" v-if="loading"></app-loader>
             </div>
             <kiss-svg :src="$base('assets:assets/icons/video.svg')" width="80" height="80" v-else-if="asset.type=='video'"></kiss-svg>
