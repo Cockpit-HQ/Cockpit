d261b9da14b4a17de1e3015c4db3f270ab23b28f
Artur Heinze <artur@agentejo.com>
2021-12-19 23:51:48 +0100

    update vue

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_VERSION', '2.0-dev-2021-12-16');
+define('APP_VERSION', '2.0-dev-2021-12-19');
 
 if (!defined('APP_START_TIME')) define('APP_START_TIME', microtime(true));
 if (!defined('APP_CLI')) define('APP_CLI', PHP_SAPI == 'cli');
