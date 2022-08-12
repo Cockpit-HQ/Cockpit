0d254f7bbadb7f45ca04d2100a743cefe07d5565
Raffael <github@raffaeljesche.de>
2022-08-12 18:17:24 +0200

    now login modal works with 2FA enabled
    needs cleanup

--- modules/App/assets/dialog/login.js
+++ modules/App/assets/dialog/login.js
@@ -3,11 +3,11 @@ export default {
 
     data() {
         return  {
-            auth: {
-                user: '',
-                password: ''
-            },
-            loading: false
+            auth: {user:'',password:''},
+            view: 'form',
+            loading: false,
+            user: null,
+            twofaCode: null,
         }
     },
 
@@ -20,7 +20,7 @@ export default {
 
             <div class="kiss-size-small kiss-text-caption kiss-text-bold kiss-margin-bottom">{{ t('Session expired') }}</div>
 
-            <form class="app-login-form animated" :class="{'kiss-disabled': loading}" @submit.prevent="login">
+            <form class="app-login-form animated" :class="{'kiss-disabled': loading}" @submit.prevent="login" v-if="view=='form'">
 
                 <div class="kiss-margin">
                     <label for="auth_username">{{ t('Username or Email') }}</label>
@@ -37,6 +37,32 @@ export default {
                 </div>
 
             </form>
+
+            <div class="kiss-padding animated fadeIn" :class="{'kiss-disabled': loading}" v-if="view=='success' && user.twofa">
+
+                <kiss-card class="kiss-padding-small" theme="contrast">
+
+                    <kiss-row class="kiss-flex-middle">
+                        <div><app-avatar size="50" :name="user.name"></app-avatar></div>
+                        <div class="kiss-size-small">
+                            <div class="kiss-text-bold">{{ user.name }}</div>
+                            <div class="kiss-color-muted">{{ user.email }}</div>
+                        </div>
+                    </kiss-row>
+
+                </kiss-card>
+
+                <form class="kiss-margin-top" @submit.prevent="verify2FA" v-if="!loading">
+                    <div>
+                        <label for="auth_2fa">2FA Code:</label>
+                        <input id="auth_2fa" class="kiss-input" type="text" placeholder="Code" v-model="twofaCode" autofocus required>
+                    </div>
+                    <div class="kiss-margin-top">
+                        <button class="kiss-button kiss-button-outline kiss-button-primary kiss-width-1-1">{{ t('Verify code') }}</button>
+                    </div>
+                </form>
+
+            </div>
 
         </div>
     `,
@@ -74,14 +100,50 @@ export default {
                     return;
                 }
 
-                AppEventStream.start();
+                this.user = rsp.user;
+                this.view = 'success';
 
-                this.$close();
+                // close modal if twofa is disabled for the user
+                if (!rsp.user.twofa) {
+                    AppEventStream.start();
+                    this.$close();
+                }
 
             }, rsp => {
                 this.loading = false;
                 App.ui.notify(rsp && (rsp.message || rsp.error) ? (rsp.message || rsp.error) : this.t('Login failed.'), 'error');
             });
+        },
+
+        verify2FA() {
+
+            this.loading = true;
+
+            this.$request('/auth/validate2FA', {
+                code: this.twofaCode,
+                token : this.user.twofa
+            }).then(rsp => {
+
+                if (rsp && rsp.success) {
+                    this.loading = false;
+
+                    AppEventStream.start();
+                    this.$close();
+                    return;
+                }
+
+            }, rsp => {
+
+                this.loading = false;
+
+                App.ui.notify(rsp && (rsp.message || rsp.error) ? (rsp.message || rsp.error) : t('Verification failed.'), 'error');
+
+                setTimeout(() => {
+                    this.$el.parentNode.classList.add('animated');
+                    this.$el.parentNode.classList.add('shake');
+                }, 100);
+            });
+
         }
     }
 }
