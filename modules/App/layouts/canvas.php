b181aafc3196b6f991dd99bf686099542a9ec991
Artur Heinze <artur@agentejo.com>
2022-08-23 22:01:36 +0200

    add  noindex, nofollow meta for bots

--- modules/App/layouts/canvas.php
+++ modules/App/layouts/canvas.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex,nofollow">
     <title><?=$this->helper('theme')->title()?></title>
 
     <link rel="icon" href="<?=$this->helper('theme')->favicon()?>">
