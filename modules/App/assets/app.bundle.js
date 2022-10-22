4711e189aeeb31d3934aba6963160090ae7025b8
Artur Heinze <artur@agentejo.com>
2022-10-23 00:54:49 +0200

    cleanup

--- modules/App/assets/app.bundle.js
+++ modules/App/assets/app.bundle.js
@@ -3272,10 +3272,10 @@
           let colours = [
               "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50",
               "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
-          ],
+          ];
 
-              nameSplit = String(name).toUpperCase().split(' '),
-              initials, charIndex, colourIndex, context, dataURI;
+          let nameSplit = String(name).toUpperCase().split(' ');
+          let initials, charIndex, colourIndex, context, dataURI;
 
 
           if (nameSplit.length == 1) {
