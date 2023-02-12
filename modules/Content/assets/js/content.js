dc1ad40d9934670d0c16497db464d670e23d7630
Artur Heinze <artur@agentejo.com>
2023-02-12 13:10:33 +0100

    cleanup

--- modules/Content/assets/js/content.js
+++ modules/Content/assets/js/content.js
@@ -2,6 +2,9 @@
 VueView.component('field-contentItemLink', 'content:assets/vue-components/field-content-item-link.js');
 VueView.component('options-linkModel', 'content:assets/vue-components/options-link-model.js');
 
-App.utils.$interpolate.fns.$content = function(model, id, display) {
-    return `<display-content class="kiss-display-inline-block" model="${model || ''}" id="${id || ''}" display="${display || ''}"></display-content>`;
+App.utils.$interpolate.fns.$content = function(link, display) {
+
+    if (!link || !link._model || !link._id) return '';
+
+    return `<display-content class="kiss-display-inline-block" model="${link._model}" id="${link._id}" display="${display}"></display-content>`;
 };
