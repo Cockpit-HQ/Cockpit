598182310ee4d95001d0dcbcc2a11a88ab160148
Artur Heinze <artur@agentejo.com>
2023-03-08 22:06:40 +0100

    Bump version to 2.4.0

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('APP_VERSION', '2.3.9');
+define('APP_VERSION', '2.4.0');
 
 if (!defined('APP_START_TIME')) define('APP_START_TIME', microtime(true));
 if (!defined('APP_ADMIN')) define('APP_ADMIN', false);
