218d477f160c11632de65577b07733e79c85ea33
Artur Heinze <artur@agentejo.com>
2024-01-29 23:35:44 +0100

    cleanup

--- modules/App/views/dashboard/index.php
+++ modules/App/views/dashboard/index.php
@@ -15,7 +15,7 @@
     <div class="kiss-height-50vh kiss-flex kiss-flex-middle kiss-flex-center kiss-align-center">
         <div class="animated fadeInUp">
             <div class="kiss-size-xlarge kiss-margin-small"><?=t('Hello.')?></div>
-            <div class="kiss-color-muted kiss-size-1 kiss-text-light animated fadeIn delay-1s"><?=t("Excited to see what you'll create today!")?></div>
+            <div class="kiss-color-muted kiss-size-1 kiss-text-light animated fadeIn delay-1s"><?=t("Excited for your creations today!")?></div>
         </div>
     </div>
 
