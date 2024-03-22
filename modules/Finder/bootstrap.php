10d5395bea5c01a07e7147bf77c3593373836c3b
Artur Heinze <artur@agentejo.com>
2024-03-22 13:33:54 +0100

    Add finder.disabled setting

--- modules/Finder/bootstrap.php
+++ modules/Finder/bootstrap.php
@@ -3,6 +3,10 @@
 // load admin related code
 $this->on('app.admin.init', function() {
 
+    if ($this->retrieve('finder.disabled') === true) {
+        return;
+    }
+
     include(__DIR__.'/admin.php');
 
 }, -1000);
