8cf766d5277b4b18cb02bd93a37f086003767c3e
Artur Heinze <artur@agentejo.com>
2024-07-04 22:35:30 +0200

    update vendor libs

--- lib/vendor/zircote/swagger-php/src/Generator.php
+++ lib/vendor/zircote/swagger-php/src/Generator.php
@@ -219,12 +219,24 @@ class Generator
                 $value = 'true' == $value;
             }
 
+            if ($isList = ('[]' == substr($key, -2))) {
+                $key = substr($key, 0, -2);
+            }
             $token = explode('.', $key);
             if (2 == count($token)) {
                 // 'operationId.hash' => false
-                $normalised[$token[0]][$token[1]] = $value;
+                // namespaced / processor
+                if ($isList) {
+                    $normalised[$token[0]][$token[1]][] = $value;
+                } else {
+                    $normalised[$token[0]][$token[1]] = $value;
+                }
             } else {
-                $normalised[$key] = $value;
+                if ($isList) {
+                    $normalised[$key][] = $value;
+                } else {
+                    $normalised[$key] = $value;
+                }
             }
         }
 
@@ -263,7 +275,10 @@ class Generator
                 new Processors\MergeJsonContent(),
                 new Processors\MergeXmlContent(),
                 new Processors\OperationId(),
+                new Processors\AugmentTags(),
                 new Processors\CleanUnmerged(),
+                new Processors\PathFilter(),
+                new Processors\CleanUnusedComponents(),
             ]);
         }
 
@@ -422,6 +437,7 @@ class Generator
                 'analysis' => null,
                 'processor' => null,
                 'processors' => null,
+                'config' => [],
                 'logger' => null,
                 'validate' => true,
                 'version' => null,
@@ -436,6 +452,7 @@ class Generator
             ->setNamespaces($config['namespaces'])
             ->setAnalyser($config['analyser'])
             ->setProcessorPipeline($processorPipeline)
+            ->setConfig($config['config'])
             ->generate($sources, $config['analysis'], $config['validate']);
     }
 
--- lib/vendor/zircote/swagger-php/src/Processors/AugmentParameters.php
+++ lib/vendor/zircote/swagger-php/src/Processors/AugmentParameters.php
@@ -11,11 +11,14 @@ use OpenApi\Annotations as OA;
 use OpenApi\Generator;
 use OpenApi\Processors\Concerns\DocblockTrait;
 
+/**
+ * Augments shared and operations parameters from docblock comments.
+ */
 class AugmentParameters implements ProcessorInterface
 {
     use DocblockTrait;
 
-    protected $augmentOperationParameters;
+    protected $augmentOperationParameters = true;
 
     public function __construct(bool $augmentOperationParameters = true)
     {
@@ -30,9 +33,11 @@ class AugmentParameters implements ProcessorInterface
     /**
      * If set to <code>true</code> try to find operation parameter descriptions in the operation docblock.
      */
-    public function setAugmentOperationParameters(bool $augmentOperationParameters): void
+    public function setAugmentOperationParameters(bool $augmentOperationParameters): AugmentParameters
     {
         $this->augmentOperationParameters = $augmentOperationParameters;
+
+        return $this;
     }
 
     public function __invoke(Analysis $analysis)
