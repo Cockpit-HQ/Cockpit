f0b336286dad3c61193118050d6245f8de998075
Artur Heinze <artur@agentejo.com>
2024-10-29 00:34:50 +0100

    use official VueDraggablePlus built

--- modules/App/assets/js/components.js
+++ modules/App/assets/js/components.js
@@ -11,7 +11,7 @@ import "../../../System/assets/js/components.js";
 // General
 VueView.component('vue-draggable', Vue.defineAsyncComponent(() => {
     return new Promise(resolve => {
-        App.assets.require(['app:assets/vendor/vue/components/vue-draggable.js']).then(() => resolve(window.VueDraggable.VueDraggable));
+        App.assets.require(['app:assets/vendor/vue/components/vue-draggable.js']).then(() => resolve(window.VueDraggablePlus.VueDraggable));
     })
 }));
 
