2658e19566aafcc171dc31c68f4ba6dcb1a75c78
Artur Heinze <artur@agentejo.com>
2024-11-11 04:50:24 +0100

    cleanup

--- modules/App/Helper/Async.php
+++ modules/App/Helper/Async.php
@@ -31,16 +31,16 @@ class Async extends \Lime\Helper {
 
     public function exec($script, $params = [], $maxTime = 60) {
 
-        $processId = \uniqid('worker').'-'.(\time() + $maxTime);
+        $processId = uniqid('worker').'-'.(time() + $maxTime);
         $fs = $this->app->helper('fs');
 
         if ($path = $this->app->path($script)) {
-            $script = \file_get_contents($path);
+            $script = file_get_contents($path);
         }
 
-        $script = \trim($script);
+        $script = trim($script);
 
-        if (\substr($script, 0, 4) !== '<?php') {
+        if (substr($script, 0, 4) !== '<?php') {
             $script = "<?php ".$script;
         }
 
@@ -67,14 +67,14 @@ function Cockpit() {
     static \$instance;
 
     if (!isset(\$instance)) {
-        \$instance = Cockpit::instance('{$envDir}', ".\var_export($envAppVars, true).");
+        \$instance = Cockpit::instance('{$envDir}', ".var_export($envAppVars, true).");
         \$GLOBALS['APP'] = \$instance;
     }
 
     return \$instance;
 }
 
-extract(".\var_export($params, true).");
+extract(".var_export($params, true).");
 
 ?>".$script."
 
@@ -93,16 +93,16 @@ unlink(__FILE__);
 
     public function finished($processId, &$error = null) {
 
-        $processId = \str_replace('..', '', $processId);
+        $processId = str_replace('..', '', $processId);
         $file = $this->app->path("#storage:tmp/async/{$processId}.php");
 
         if ($file) {
-            $exit = \explode('-', basename($file, '.php'))[1];
+            $exit = explode('-', basename($file, '.php'))[1];
 
-            if (\time() > $exit) {
+            if (time() > $exit) {
 
                 // do something
-                \unlink($file);
+                unlink($file);
                 $error = 'timeout';
                 return true;
             }
@@ -119,8 +119,8 @@ unlink(__FILE__);
 
             // fire and forget calling script
             $url   = $this->app->pathToUrl($scriptfile, true).'?async=true';
-            $parts = \parse_url($url);
-            $fp    = \fsockopen($parts['host'], isset($parts['port']) ? $parts['port']:80, $errno, $errstr, 30);
+            $parts = parse_url($url);
+            $fp    = fsockopen($parts['host'], isset($parts['port']) ? $parts['port']:80, $errno, $errstr, 30);
             $out   = '';
 
             if ($fp) {
@@ -132,33 +132,33 @@ unlink(__FILE__);
                 if (isset($parts['query'])) $out.= $parts['query'];
             }
 
-            \fwrite($fp, $out);
-            \fclose($fp);
+            fwrite($fp, $out);
+            fclose($fp);
             return;
         }
 
         $cmd = $this->phpPath." -f $scriptfile";
 
-        if (\substr(\php_uname(), 0, 7) == "Windows") {
-            \pclose(popen("start /B ". $cmd, "r"));
+        if (substr(php_uname(), 0, 7) == 'Windows') {
+            pclose(popen("start /B ". $cmd, "r"));
         } else {
-            \exec($cmd . " > /dev/null &");
+            exec($cmd . " > /dev/null &");
         }
     }
 
     protected function isExecAvailable() {
 
-        if (!$this->phpPath || \in_array(\strtolower(\ini_get('safe_mode')), ['on', '1'], true) || (!\function_exists('exec'))) {
+        if (!$this->phpPath || in_array(strtolower(ini_get('safe_mode')), ['on', '1'], true) || (!function_exists('exec'))) {
             return false;
         }
 
-        $disabled_functions = \explode(',', \ini_get('disable_functions'));
+        $disabled_functions = explode(',', ini_get('disable_functions'));
 
-        return !\in_array('exec', $disabled_functions) && strlen(trim(exec($this->phpPath.' -v')));
+        return !in_array('exec', $disabled_functions) && strlen(trim(exec($this->phpPath.' -v')));
     }
 
     public function possible() {
-        return $this->isExecAvailable() || \function_exists('fsockopen');
+        return $this->isExecAvailable() || function_exists('fsockopen');
     }
 
 }
