8b5108a487c60ffbf43d5744a66d4bc9984c5bf1
Artur Heinze <artur@agentejo.com>
2025-03-05 17:29:20 +0100

    update vendor libs

--- lib/vendor/aws/aws-sdk-php/src/Sdk.php
+++ lib/vendor/aws/aws-sdk-php/src/Sdk.php
@@ -378,6 +378,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionIoTFleetWise(array $args = [])
  * @method \Aws\IoTJobsDataPlane\IoTJobsDataPlaneClient createIoTJobsDataPlane(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionIoTJobsDataPlane(array $args = [])
+ * @method \Aws\IoTManagedIntegrations\IoTManagedIntegrationsClient createIoTManagedIntegrations(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionIoTManagedIntegrations(array $args = [])
  * @method \Aws\IoTSecureTunneling\IoTSecureTunnelingClient createIoTSecureTunneling(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionIoTSecureTunneling(array $args = [])
  * @method \Aws\IoTSiteWise\IoTSiteWiseClient createIoTSiteWise(array $args = [])
@@ -809,7 +811,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.340.4';
+    const VERSION = '3.341.0';
 
     /** @var array Arguments for creating clients */
     private $args;
