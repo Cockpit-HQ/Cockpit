6202d26c38abfbeec94be086da819ac4466bc1de
Artur Heinze <artur@agentejo.com>
2025-12-10 02:22:19 +0100

    cleanup

--- modules/System/assets/dialogs/graphql-viewer.js
+++ modules/System/assets/dialogs/graphql-viewer.js
@@ -1,6 +1,6 @@
 export default {
 
-    _meta: {flip: true, size: 'xxlarge'},
+    _meta: { flip: true, size: 'xxlarge' },
 
     data() {
 
@@ -31,6 +31,9 @@ export default {
     template: /*html*/`
 
         <div class="app-offcanvas-container">
+            <div class="kiss-padding kiss-text-bold">
+                {{ t('GraphQL Playground') }}
+            </div>
             <div class="app-offcanvas-content kiss-position-relative kiss-bgcolor-contrast kiss-flex-1 kiss-flex kiss-flex-middle kiss-flex-center">
                 <div v-if="!loaded"><app-loader></app-loader></div>
                 <iframe :src="iframeSrc" style="position:absolute;top:0;left:0;width:100%;height:100%;" @load="loaded=true"></iframe>
